9bade48ddb4ff7eb05a697f293a6d64a18a52b82
3rdFloorTom <thomas.laughliniii@gmail.com>
2020-08-03 08:47:30 -0700

    sign error fix and tidy-up

--- tbl2em.m
+++ tbl2em.m
@@ -1,5 +1,6 @@
-%%% Converts a dynamo table (.tbl) file into an .em formatted file
-%%% for visualization in UCSF chimera using the Place Object plugin
+%%% Converts a dynamo table (.tbl) file into an AV3 em formatted file
+%%% for a given tomogram for visualization in UCSF chimera using the 
+%%% Place Object plugin
 %%% https://www2.mrc-lmb.cam.ac.uk/groups/briggs/resources/place-object/
 %%%
 %%% Usage:
@@ -18,25 +19,29 @@ tomoTbl = dynamo_table_grep(fullTbl,'tomo',tomoIndex);
 % Initialize empty matrix to build-up
 emtrx = zeros(20,length(tomoTbl(:,1)));
             
+% Fill matrix
 emtrx(1,:) = tomoTbl(:,10);     % CC
 emtrx(4,:) = tomoTbl(:,1);      % ptcl tag/number
-emtrx(5,:) = tomoIndex;
+emtrx(5,:) = tomoIndex;         % tomogram index
 emtrx(7,:) = tomoIndex;
-emtrx(6,:) = tomoTbl(:,22);     % class/model
+emtrx(6,:) = tomoTbl(:,21);     % tomogram sub-area
 emtrx(8,:) = tomoTbl(:,24);     % X-coordinate in full tomogram
 emtrx(9,:) = tomoTbl(:,25);     % Y-coordinate in full tomogram
 emtrx(10,:) = tomoTbl(:,26);    % Z-coordinate in full tomogram
 emtrx(11,:) = tomoTbl(:,4);     % X-shift in subvolume
 emtrx(12,:) = tomoTbl(:,5);     % Y-shift in subvolume
 emtrx(13,:) = tomoTbl(:,6);     % Z-shift in subvolume
-emtrx(17,:) = tomoTbl(:,9);     % Phi/narot/Z' rotation
-emtrx(18,:) = tomoTbl(:,7);     % Psi/tdrot/Z rotation
-emtrx(19,:) = tomoTbl(:,8);     % Theta/tilt/X rotation
-emtrx(20,:) = tomoTbl(:,34);    % reference
+emtrx(17,:) = -tomoTbl(:,9);    % Phi/narot/Z' rotation
+emtrx(18,:) = -tomoTbl(:,7);    % Psi/tdrot/Z rotation
+emtrx(19,:) = -tomoTbl(:,8);    % Theta/tilt/X rotation
+emtrx(20,:) = tomoTbl(:,22);    % Class
 
-emfile = sprintf('%s_tomo_%d.em',tableFile,tomoIndex);
-%writematrix(emtrx,emfile,'Delimiter','comma');
+% Generate name of output .em file and write to disk with dynamo function
+tableFileRootname = strrep(tableFile,'.tbl','');
+emfile = sprintf('%s_tomo_%d.em',tableFileRootname,tomoIndex);
 dwrite(emtrx,emfile);
+
+% Print completion to stdout
 fprintf('Wrote out file: %s\n', emfile)
 
 
